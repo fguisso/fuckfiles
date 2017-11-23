74590c415c0b658f722f2674d70d5fab2460e123
Fernando Guisso <fguisso@gmail.com>
2017-11-23 11:42:57 -0200

    add gitignore

--- .index.sh
+++ .index.sh
@@ -1,2 +1,5 @@
 source ~/dotfiles/.alias.sh
 source ~/dotfiles/.powerlevel9k.sh
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
