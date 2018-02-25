9da6e6f524591ad9e8e6ed2faef5750a268f7601
Fernando Guisso <fguisso@gmail.com>
2018-02-25 04:22:33 -0300

    add env variables

--- .index.sh
+++ .index.sh
@@ -1,5 +1,6 @@
 source ~/fuckfiles/.alias.sh
 source ~/fuckfiles/.powerlevel9k.sh
+source ~/fuckfiles/.env.sh
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
